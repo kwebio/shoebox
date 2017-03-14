846e993af6284544dccf847b6cefeda0c1030bca
Ian Clarke <github@ian.33mail.com>
2017-03-14 10:12:47 -0500

    remove outdated test

--- src/test/kotlin/com/github/sanity/shoebox/UtilsKtSpec.kt
+++ src/test/kotlin/com/github/sanity/shoebox/UtilsKtSpec.kt
@@ -7,17 +7,5 @@ import io.kotlintest.specs.FreeSpec
  */
 class UtilsKtSpec : FreeSpec() {
     init {
-        "identityRemove should" - {
-            val list = ArrayList<String>()
-            val v1 = "hello"
-            val v2 = "hell"+"o"
-            val v3 = v1
-            (v1 === v2) shouldBe false
-            (v1 === v3) shouldBe true
-            list.add(v1)
-            list.add(v2)
-            list.add(v3)
-
-        }
     }
 }
