9e07f8ce543ea51f7da4e03f254367416b7154eb
Ian Clarke <ian.clarke@gmail.com>
2019-11-01 22:12:43 -0500

    more work

--- src/main/kotlin/io/kweb/shoebox/OrderedViewSet.kt
+++ src/main/kotlin/io/kweb/shoebox/OrderedViewSet.kt
@@ -8,25 +8,24 @@ import java.util.concurrent.CopyOnWriteArrayList
  * Created by ian on 3/14/17.
  */
 
-class OrderedViewSet<T : Any>(val view : View<T>, val viewKey : String, val comparator: Comparator<T>) {
+class OrderedViewSet<T : Any>(val view: View<T>, val viewKey: String, val comparator: Comparator<T>) {
 
-    private val orderedList : CopyOnWriteArrayList<KeyValue<T>>
+    private val orderedList: CopyOnWriteArrayList<KeyValue<T>>
     private val modificationHandlers = ConcurrentHashMap<String, Long>()
     private val additionHandle: Long
     private val removalHandle: Long
 
     init {
-        val ol = CopyOnWriteArrayList<KeyValue<T>>()
-        val kvComparator : Comparator<KeyValue<T>> = Comparator<KeyValue<T>> { o1, o2 -> comparator.compare(o1.value, o2.value) }.thenBy(KeyValue<T>::key)
-        ol.addAll(view.getKeyValues(viewKey))
-        ol.sortWith(kvComparator)
-        orderedList = ol
+        orderedList = CopyOnWriteArrayList<KeyValue<T>>()
+        val kvComparator: Comparator<KeyValue<T>> = Comparator<KeyValue<T>> { o1, o2 -> comparator.compare(o1.value, o2.value) }.thenBy(KeyValue<T>::key)
+        orderedList.addAll(view.getKeyValues(viewKey))
+        orderedList.sortWith(kvComparator)
         additionHandle = view.onAdd(viewKey) { keyValue ->
             val insertionPoint: Int = when (val binarySearchResult = orderedList.betterBinarySearch(keyValue, kvComparator)) {
                 is Exact -> binarySearchResult.index
                 is Between -> binarySearchResult.highIndex
             }
-            ol.add(insertionPoint, keyValue)
+            orderedList.add(insertionPoint, keyValue)
             insertListeners.values.forEach {
                 try {
                     it(insertionPoint, keyValue)
@@ -53,12 +52,11 @@ class OrderedViewSet<T : Any>(val view : View<T>, val viewKey : String, val comp
             }
         }
 
-        ol.forEach { kv ->
+        // This isn't right, are modification handlers being added for new members of the Set?
+        //
+        orderedList.forEach { kv ->
             modificationHandlers[kv.key] = view.viewOf.onChange(kv.key) { oldValue, newValue, _ ->
                 if (comparator.compare(oldValue, newValue) != 0) {
-                    for (modifyListener in modifyListeners.values) {
-                        modifyListener.invoke(oldValue, newValue)
-                    }
                     val newKeyValue = KeyValue(kv.key, newValue)
                     val insertionIndex: Int = when (val insertPoint = orderedList.betterBinarySearch(newKeyValue, kvComparator)) {
                         is Exact -> insertPoint.index
@@ -67,56 +65,65 @@ class OrderedViewSet<T : Any>(val view : View<T>, val viewKey : String, val comp
                     insertListeners.values.forEach { it(insertionIndex, newKeyValue) }
 
                     val oldKeyValue = KeyValue(kv.key, oldValue)
+
+                    orderedList.add(insertionIndex, newKeyValue)
+
                     when (val removePoint = orderedList.betterBinarySearch(oldKeyValue, kvComparator)) {
-                        is Exact -> removeListeners.values.forEach { it(removePoint.index, oldKeyValue) }
+                        is Exact -> {
+                            orderedList.removeAt(removePoint.index)
+                            removeListeners.values.forEach { it(removePoint.index, oldKeyValue) }
+                        }
                     }
 
+                for (modifyListener in modifyListeners.values) {
+                    modifyListener.invoke(oldValue, newValue)
                 }
             }
         }
     }
+}
 
-    private val insertListeners = ConcurrentHashMap<Long, (Int, KeyValue<T>) -> Unit>()
-    private val removeListeners = ConcurrentHashMap<Long, (Int, KeyValue<T>) -> Unit>()
-    private val modifyListeners = ConcurrentHashMap<Long, (old : T, new : T) -> Unit>()
+private val insertListeners = ConcurrentHashMap<Long, (Int, KeyValue<T>) -> Unit>()
+private val removeListeners = ConcurrentHashMap<Long, (Int, KeyValue<T>) -> Unit>()
+private val modifyListeners = ConcurrentHashMap<Long, (old: T, new: T) -> Unit>()
 
-    val entries : List<T> get() = keyValueEntries.map(KeyValue<T>::value)
+val entries: List<T> get() = keyValueEntries.map(KeyValue<T>::value)
 
-    val keyValueEntries : List<KeyValue<T>> = orderedList
+val keyValueEntries: List<KeyValue<T>> = orderedList
 
-    fun onInsert(listener : (Int, KeyValue<T>) -> Unit) : Long {
-        val handle = listenerHandleSource.incrementAndGet()
-        insertListeners[handle] = listener
-        return handle
-    }
+fun onInsert(listener: (Int, KeyValue<T>) -> Unit): Long {
+    val handle = listenerHandleSource.incrementAndGet()
+    insertListeners[handle] = listener
+    return handle
+}
 
-    fun deleteInsertListener(handle : Long) {
-        insertListeners.remove(handle)
-    }
+fun deleteInsertListener(handle: Long) {
+    insertListeners.remove(handle)
+}
 
-    fun onRemove(listener : (Int, KeyValue<T>) -> Unit) : Long {
-        val handle = listenerHandleSource.incrementAndGet()
-        removeListeners[handle] = listener
-        return handle
-    }
+fun onRemove(listener: (Int, KeyValue<T>) -> Unit): Long {
+    val handle = listenerHandleSource.incrementAndGet()
+    removeListeners[handle] = listener
+    return handle
+}
 
-    fun onModify(listener : (old : T, new : T) -> Unit) : Long {
-        val handle = listenerHandleSource.incrementAndGet()
-        modifyListeners[handle] = listener
-        return handle
-    }
+fun onModify(listener: (old: T, new: T) -> Unit): Long {
+    val handle = listenerHandleSource.incrementAndGet()
+    modifyListeners[handle] = listener
+    return handle
+}
 
-    fun deleteModifyListener(handle : Long) {
-        modifyListeners.remove(handle)
-    }
+fun deleteModifyListener(handle: Long) {
+    modifyListeners.remove(handle)
+}
 
-    fun deleteRemoveListener(handle : Long) {
-        removeListeners.remove(handle)
-    }
+fun deleteRemoveListener(handle: Long) {
+    removeListeners.remove(handle)
+}
 
-    protected fun finalize() {
-        view.deleteAddListener(viewKey, additionHandle)
-        view.deleteRemoveListener(viewKey, removalHandle)
-        modificationHandlers.forEach { (key, handler) -> view.viewOf.deleteChangeListener(key, handler) }
-    }
+protected fun finalize() {
+    view.deleteAddListener(viewKey, additionHandle)
+    view.deleteRemoveListener(viewKey, removalHandle)
+    modificationHandlers.forEach { (key, handler) -> view.viewOf.deleteChangeListener(key, handler) }
+}
 }
