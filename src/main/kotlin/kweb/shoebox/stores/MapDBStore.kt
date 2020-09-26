5ba1d72843fad32acbc3c7f786ca29f7e02fbbeb
Ian Clarke <ian.clarke@gmail.com>
2020-09-26 14:39:06 -0500

    initial work, doesn't compile

--- src/main/kotlin/kweb/shoebox/stores/MapDBStore.kt
+++ src/main/kotlin/kweb/shoebox/stores/MapDBStore.kt
@@ -8,24 +8,30 @@ import kotlinx.serialization.protobuf.ProtoBuf
 import kweb.shoebox.KeyValue
 import kweb.shoebox.Store
 import org.mapdb.DB
+import org.mapdb.HTreeMap
 import org.mapdb.Serializer
+import kotlin.reflect.KClass
 
 /**
  * Created by ian on 3/22/17.
  */
 @ExperimentalSerializationApi
-class MapDBStore<T : Any>(val db : DB, val name : String, val serializer: KSerializer<T>) : Store<T> {
+class MapDBStore<T : Any>(val db: DB, val name: String, val serializer: KSerializer<T>, val format: Format<Any>) : Store<T> {
 
-    private val map =
+    private val map: HTreeMap<String, Any> =
             db
                     .hashMap(name)
                     .keySerializer(Serializer.STRING)
-                    .valueSerializer(Serializer.STRING)
-                    .createOrOpen()
+                    .valueSerializer(when(format.format) {
+                        String::class -> Serializer.STRING
+                        ByteArray::class -> Serializer.BYTE_ARRAY
+                        else -> error("Only String and ByteArray serialization formats are supported, ${format.format} is not.")
+                    })
+                    .createOrOpen() as HTreeMap<String, Any>
 
     override val entries: Iterable<KeyValue<T>>
         get() = map.map { (k, v) ->
-            KeyValue(k, Json.decodeFromString(serializer, v))
+            KeyValue(k, format.deserialize(serializer, v))
         }
 
     override fun remove(key: String): T? {
@@ -33,7 +39,7 @@ class MapDBStore<T : Any>(val db : DB, val name : String, val serializer: KSeria
         return if (v == null) {
             null
         } else {
-            Json.decodeFromString(serializer, v)
+            format.deserialize(serializer, v)
         }
     }
 
@@ -48,11 +54,44 @@ class MapDBStore<T : Any>(val db : DB, val name : String, val serializer: KSeria
 
     override fun set(key: String, value: T): T? {
         val v = map[key]
-        map.set(key, Json.encodeToString(serializer, value))
+        map.set(key, format.serialize(serializer, value) as Any)
         return if (v == null) {
             null
         } else {
             Json.decodeFromString(serializer, v)
         }
     }
+
+    interface Format<O : Any> {
+        val format: KClass<O>
+
+        fun <T : Any> serialize(serializer: KSerializer<T>, value: T): O
+
+        fun <T : Any> deserialize(serializer: KSerializer<T>, data: O): T
+    }
+
+    class JsonFormat : Format<String> {
+        override val format = String::class
+
+        override fun <T : Any> serialize(serializer: KSerializer<T>, value: T): String = Json.encodeToString(serializer, value)
+
+        override fun <T : Any> deserialize(serializer: KSerializer<T>, data: String): T = Json.decodeFromString(serializer, data)
+    }
+
+    class ProtoBufFormat : Format<ByteArray> {
+        private val protoBuf = ProtoBuf { encodeDefaults = false }
+
+        override val format = ByteArray::class
+
+        override fun <T : Any> serialize(serializer: KSerializer<T>, value: T): ByteArray = protoBuf.encodeToByteArray(serializer, value)
+
+        override fun <T : Any> deserialize(serializer: KSerializer<T>, data: ByteArray): T =protoBuf.decodeFromByteArray(serializer, data)
+    }
+}
+
+fun main() {
+    @Serializable
+    data class Bar(val k: Int? = null)
+
+    val s = ProtoBuf { encodeDefaults = false }.encodeToByteArray(Bar.serializer(), Bar(null))
 }
