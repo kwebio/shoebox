8fc8c1636d321990a928ca83ef1a04c3e8c85599
Ian Clarke <ian.clarke@gmail.com>
2021-05-07 15:15:14 -0500

    fix dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.0"
+    id("org.jetbrains.dokka") version "1.4.32"
     `maven-publish`
     kotlin("plugin.serialization") version "1.5.0"
     id("com.github.ben-manes.versions") version "0.38.0"
